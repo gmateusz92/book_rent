3234e0ca2936a281ca922dbf79cc5117b3466cb7
gmateusz92 <gmateusz92@gmail.com>
2023-10-27 22:56:30 +0200

    1

--- book_rent/settings.py
+++ book_rent/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'book_rent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
