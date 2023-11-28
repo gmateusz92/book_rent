75c903b74c990c383fb213677b1b446a9cc6afb5
gmateusz92 <gmateusz92@gmail.com>
2023-11-28 15:16:24 +0100

    1

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
      search_book_view, 
-#     # BookRentalHistoryView, 
+     BookRentalHistoryView, 
 #     # UpdateRentalStatusView,
 #     # CreateNewRentalView,
 #     # SelectDownloadRentalsView
@@ -11,7 +11,7 @@ app_name = 'rentals'
 
 urlpatterns = [
     path('', search_book_view, name='main'),
-    # path('<str:book_id>/', BookRentalHistoryView.as_view(), name="detail"),
+    path('<str:book_id>/', BookRentalHistoryView.as_view(), name="detail"),
     # path('<str:book_id>/new/', CreateNewRentalView.as_view(), name="new"),
     # path('<str:book_id>/download/', SelectDownloadRentalsView.as_view(), name="download"),
     # path('<str:book_id>/<pk>/', UpdateRentalStatusView.as_view(), name="update"),
--- rentals/views.py
+++ rentals/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import SearchBookForm
 from books.models import Book
+from django.views.generic import ListView, UpdateView, CreateView, FormView
+from .models import Rental
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def search_book_view(request):
     form = SearchBookForm(request.POST or None)
@@ -8,10 +11,40 @@ def search_book_view(request):
     book_ex = Book.objects.filter(isbn=search_query).exists()
 
     if search_query is not None and book_ex:
-    #     return redirect('rentals:detail', search_query)
-        pass
+        return redirect('rentals:detail', search_query)
+        
 
     context = {
          'form': form,
     }
     return render(request, 'rentals/main.html', context)
+
+class BookRentalHistoryView(LoginRequiredMixin, ListView):
+    model = Rental # Rental.objects.all()
+    template_name = 'rentals/detail.html'
+
+    # def get_queryset(self):
+    #     book_id = self.kwargs.get('book_id')
+    #     return Rental.objects.filter(Q(book__isbn=book_id) | Q(book__id=book_id))
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     book_id = self.kwargs.get('book_id')
+    #     obj = Book.objects.filter(Q(isbn=book_id) | Q(id=book_id)).first()
+    #     # obj = get_object_or_404(Book, Q(isbn=book_id) | Q(id=book_id))
+    #     context['object'] = obj
+    #     context['book_id'] = book_id
+    #     return context
+
+    def get_queryset(self):
+        book_id = self.kwargs.get('book_id')
+        return Rental.objects.filter(book__isbn=book_id)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        qs = self.get_queryset()
+        obj = None
+        if qs.exists():
+            obj = qs.first()
+        context['obj'] = obj
+        return context    
